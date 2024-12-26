980c6a8a2d097a9bf25fc92a5ceae085b1d0d011
azizTarik-ui <aziztarik816@gmail.com>
2024-12-26 18:27:06 +0600

    Sell History Details done

--- app/Http/Controllers/admin/SaleHistoryController.php
+++ app/Http/Controllers/admin/SaleHistoryController.php
@@ -58,6 +58,7 @@ class SaleHistoryController extends Controller
     public function invoice($id)
     {
         $sale = SellProduction::where('id', $id)->with('customer','production','sellHistory')->get();
+        dd($sale);
         return view('admin.pages.sale.invoice', compact('sale'));
     }
 
