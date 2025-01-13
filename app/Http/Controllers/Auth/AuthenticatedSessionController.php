2bddb37831bd206db2fd0f6a0df0d09bf827fd73
Md-Anikul-Islam <anikul.islam01@northsouth.edu>
2025-01-13 17:20:16 +0600

    first commit

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -26,36 +26,36 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
-    {
-        $request->authenticate();
-        $request->session()->regenerate();
-        return redirect()->intended(RouteServiceProvider::HOME);
-    }
-
 //    public function store(LoginRequest $request): RedirectResponse
 //    {
-//        // Authenticate the user
 //        $request->authenticate();
-//        // Regenerate session
 //        $request->session()->regenerate();
-//        // Get user information
-//        $user = Auth::user();
-//        // Store login time and other information
-//        $userAgent = new Agent();
-//        $data = [
-//            'email' => $user->email,
-//            'ip' => $request->ip(),
-//            'browser' => $userAgent->browser(),
-//            'platform' => $userAgent->platform(),
-//            'last_login' => now(), // or use your preferred date format
-//            'user_agent' => $request->header('User-Agent'),
-//        ];
-//        // Save the data to the database
-//        DB::table('login_logs')->insert($data);
-//        // Redirect the user to the intended page
 //        return redirect()->intended(RouteServiceProvider::HOME);
 //    }
+
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        // Authenticate the user
+        $request->authenticate();
+        // Regenerate session
+        $request->session()->regenerate();
+        // Get user information
+        $user = Auth::user();
+        // Store login time and other information
+        $userAgent = new Agent();
+        $data = [
+            'email' => $user->email,
+            'ip' => $request->ip(),
+            'browser' => $userAgent->browser(),
+            'platform' => $userAgent->platform(),
+            'last_login' => now(), // or use your preferred date format
+            'user_agent' => $request->header('User-Agent'),
+        ];
+        // Save the data to the database
+        DB::table('login_logs')->insert($data);
+        // Redirect the user to the intended page
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
 
 
     /**
